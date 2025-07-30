8f2c600f4414c0dd2059a4d3e9eb71029b1d6350
rayane-oubassou <rayanecollegeessays@gmail.com>
2025-07-30 23:24:23 +0100

    Adding A featured section to the home page

--- client/src/Components/HeroSection.jsx
+++ client/src/Components/HeroSection.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-import { Calendar1Icon, ClockIcon } from 'lucide-react'
+import { ArrowRight, Calendar1Icon, ClockIcon } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const HeroSection = () => {
+
+    const navigate = useNavigate()
   return (
     <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-cover bg-center h-screen'>
       <video
@@ -29,11 +32,16 @@ const HeroSection = () => {
 
   <div className="flex items-center gap-1">
     <ClockIcon className="w-5 h-5" /> 2h 19m
+    </div>
   </div>
+  <p className='max-w-md text-gray-300'>When an interdimensional rupture unravels reality, an unlikely hero must channel her newfound powers to fight bizarre and bewildering dangers from the multiverse as the fate of the world hangs in the balance.
+  </p>
+  <button onClick={()=> navigate('/movies')} className='flex items-center gap-1 px-6 py-3 text-sm bg-amber-400 hover:bg-amber-500 hover:text-black transition rounded-full transtion-colors duration-300
+  font-medium cursor-pointer'>
+    Explore Movies !
+    <ArrowRight className='w-5 h-5'/>
+  </button>
 </div>
-
-</div>
-
     </div>
   )
 }
---
+++ client/src/Components/MovieCard.jsx
@@ -0,0 +1,46 @@
+import { StarIcon } from 'lucide-react'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const MovieCard = ({movie}) => {
+  const navigate = useNavigate()
+  return (
+    <div className='flex flex-col justify-between
+
+    p-3 bg-gray-800 rounded-2x1 
+
+    hover:-translate-y-1 transition
+
+    duration-300 w-66'>
+
+      <img src={movie.backdrop_path} alt='' 
+
+      onClick={navigate(`/movies/${movie._id}`) } 
+
+      className='rounded-lg h-52 w-full object-cover 
+
+      object-right-bottom cursor-pointer'/>
+      
+      <p className='font-semibold mt-2 truncate'>{movie.title}</p>
+      
+      <p>{new Date(movie.release_date).getFullYear()} * {movie.genres.slice(0,2).map(genre=>genre.name).join("|")}
+        * {movie.runtime}
+      </p>
+      <div className='flex items-center justify-between mt-4 pb-3'>
+        <button onClick={navigate(`/movies/${movie._id}`) } className='px-4 py-2 text-xs bg-amber-300 hover:bg-amber-400 
+        transition rounded-full font-medium cursor-pointer '>Buy Tickets
+        </button>
+      <p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
+        <StarIcon className='w-4 h-4 text-amber-300 fill-amber-300'/>
+        {movie.vote_average.toFixed(1)}
+      </p>
+
+      </div>
+      
+
+
+    </div>
+  )
+}
+
+export default MovieCard
